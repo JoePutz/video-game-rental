df45f632e8965af8ac219fdc7eb05aacb7251123
Julian Leal <julianleal1997@gmail.com>
2022-06-21 12:15:42 -0500

    Api works

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React, { useEffect } from "react";
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import Rentals from "./Rentals";
-import Search from "./Search";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Rentals from "./components/Rentals";
+import Search from "./components/Search";
 
 function App() {
-  // useEffect(() =>
-  //     fetch("baseURL")
-  //     .then((r) => r.json())
-  //     .then((games) => console.log(games))
-  // )
+    const baseURL = "https://gutendex.com/books"
+    const [booksToDisplay, SetBooksToDisplay] = useState([]);
+    let allBooks = [];
+    useEffect(() => {flipPages(baseURL)}, [])
 
-  // is the callback function sent down to Search.js to retreive the Id of the game we are trying to fetch
-  function searchGame(id) {}
+    async function flipPages(booksURL){
+        await fetch(booksURL)
+        .then(r => r.json())
+        .then(booksData => {
+            booksData.results.forEach(book => {
+                allBooks.push(book)
+            });
+            console.log(allBooks)
+            //if(booksData.next !== null){
+                //flipPages(booksData.next)}
+    })}
 
-  return (
-    <div>
-      <NavBar />
-      <Switch>
-        <Route exact path="/">
-          <Home getDetails={getDetails} games={games} />
-        </Route>
-        <Route path="/rentals">
-          <Rentals />
-        </Route>
-        <Route path="/Search">
-          <Search searchGame={searchGame} />
-        </Route>
-      </Switch>
+    return (
+        <div>
+        <NavBar />
+        <Switch>
+            <Route exact path="/">
+                <Home />
+            </Route>
+            <Route path="/rentals">
+                <Rentals />
+            </Route>
+            <Route path="/Search">
+                <Search />
+            </Route>
+        </Switch>
     </div>
-  );
+    )
 }
+
 
 export default App;
