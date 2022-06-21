32274852fa169c74859c3cbe15c19b2b68e3596c
Joseph <josephputz89@gmail.com>
2022-06-21 14:41:05 -0500

    BookCard mockup

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -2,15 +2,24 @@ import React from "react";
 
 function BookCard({ book }) {
 
-    function handleSeeDetails() {
-        getDetails(book)
-    }
+    // console.log(book.formats)
+
+    // function handleSeeDetails() {
+    //     getDetails(book)
+    // }
 
     return (
         <div>
-            <h3>{book.name}</h3>
-            <img src={book.image} />
-            <button onClick={handleSeeDetails}>See Details?</button>
+            <h3>{book.title}</h3>
+            <img src={book.formats["image/jpeg"]} />
+            <p>{book.authors[0].name}</p>
+            <button>Details</button>
+            {/* The Details button should bring the viewer to 
+            /Search/id:the books id */}
+            {/* <p>{book.authors}</p> */}
+            {/* <img src={book.formats[image]} />
+            <p>{book.authors[0].name}</p> */}
+            {/* <button onClick={handleSeeDetails}>See Details?</button> */}
         </div>
     )
 }
