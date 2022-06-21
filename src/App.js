ad9b963448bc0ceb80ea3af21e84d5717e593f68
Ashley Wong <tofudragon333@gmail.com>
2022-06-21 11:26:17 -0400

    adding some JSX to various pages

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import React, { useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Home from "./components/Home";
-import Rentals from "./components/Rentals";
-import Search from "./components/Search";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import Rentals from "./Rentals";
+import Search from "./Search";
 
 function App() {
+  // useEffect(() =>
+  //     fetch("baseURL")
+  //     .then((r) => r.json())
+  //     .then((games) => console.log(games))
+  // )
 
-    // useEffect(() => 
-    //     fetch("baseURL")
-    //     .then((r) => r.json)
-    //     .then((games) => console.log(games))    
-    // )
-
-    return (
-        <div>
-        <NavBar />
-        <Switch>
-            <Route exact path="/">
-                <Home />
-            </Route>
-            <Route path="/rentals">
-                <Rentals />
-            </Route>
-            <Route path="/Search">
-                <Search />
-            </Route>
-        </Switch>
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/rentals">
+          <Rentals />
+        </Route>
+        <Route path="/Search">
+          <Search />
+        </Route>
+      </Switch>
     </div>
-    )
+  );
 }
-
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import NavBar from "./NavBar";
 
 function Home() {
+  // for the featured games
+  useEffect(() => {
+    fetch("")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <div>
       <h1>Home Page</h1>
+      <NavBar />
     </div>
   );
 }
