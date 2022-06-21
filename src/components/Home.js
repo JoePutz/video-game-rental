19148288503e846c2afbd77c43701dabb6b06aae
Ashley Wong <tofudragon333@gmail.com>
2022-06-21 15:41:56 -0400

    messing and figuring out the db.json and server stuff

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
 import React from "react";
-import BookCard from "./BookCard"
+import BookCard from "./BookCard";
 
 function Home({ getDetails, games }) {
   return (
     <div>
-      <h1>Featured Games!</h1>
+      <h1>Featured Books!</h1>
       {/* going to need a formula for top 5 or 5 random or something, 
       but for each should get <VideoGameCard game={game} getDetails={getDetails}/> */}
     </div>
